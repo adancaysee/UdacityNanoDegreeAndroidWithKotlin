8b6742e48c31d5e43be40cadf315f718f4461785
ayseadanc <adancaysee@gmail.com>
2022-11-19 21:48:16 +0300

    first commit

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,6 @@
+
+object Config  {
+    const val targetSdkVersion = 33
+    const val compileSdkVersion = 33
+    const val minSdkVersion = 19
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+
+object Libraries {
+    const val androidxCore = "androidx.core:core-ktx:1.9.0"
+    const val androidxAppcompat = "androidx.appcompat:appcompat:1.5.1"
+    const val material = "com.google.android.material:material:1.7.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+}
+
+object TestLibraries {
+    const val junit = "junit:junit:4.13.2"
+}
+
+object AndroidTestLibraries {
+    const val junit = "androidx.test.ext:junit:1.1.4"
+    const val espressoCore = "androidx.test.espresso:espresso-core:3.5.0"
+}
+
