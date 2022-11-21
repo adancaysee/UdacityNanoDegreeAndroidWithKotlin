5dd55ef9321162b60285498887a9e92c353f2771
ayseadanc <adancaysee@gmail.com>
2022-11-21 22:57:37 +0300

    about me data binding added

--- aboutme/src/main/java/com/udacity/aboutme/MainActivity.kt
+++ aboutme/src/main/java/com/udacity/aboutme/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.udacity.aboutme
 
 import android.content.Context
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
 import android.util.TypedValue.*
 import android.view.View
-import android.view.ViewConfiguration
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.udacity.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = MyName(name = "Ayse Adanc")
+
         applyDimension()
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
 
@@ -29,21 +34,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNickname(view: View) {
-        val nicknameEditText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = nicknameEditText.text
-        nicknameEditText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         //hide keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken,0)
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun applyDimension() {
-        Log.e("height",
-            TypedValue.applyDimension(COMPLEX_UNIT_DIP, 1f, resources.displayMetrics).toString())
+        Log.e(
+            "height",
+            TypedValue.applyDimension(COMPLEX_UNIT_DIP, 1f, resources.displayMetrics).toString()
+        )
     }
 }
