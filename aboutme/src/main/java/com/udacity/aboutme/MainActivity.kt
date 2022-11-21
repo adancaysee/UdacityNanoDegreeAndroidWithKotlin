769835632dd3011f5e8fea16de5e5d1f6fa1d4fc
ayseadanc <adancaysee@gmail.com>
2022-11-21 19:44:55 +0300

    about me app finished

--- aboutme/src/main/java/com/udacity/aboutme/MainActivity.kt
+++ aboutme/src/main/java/com/udacity/aboutme/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.udacity.aboutme
 
+import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.util.TypedValue.*
+import android.view.View
+import android.view.ViewConfiguration
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        applyDimension()
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+
+    }
+
+    private fun addNickname(view: View) {
+        val nicknameEditText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = nicknameEditText.text
+        nicknameEditText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
+    private fun applyDimension() {
+        Log.e("height",
+            TypedValue.applyDimension(COMPLEX_UNIT_DIP, 1f, resources.displayMetrics).toString())
     }
 }
