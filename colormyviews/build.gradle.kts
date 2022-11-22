84e31e9cd1988cdd5d64a7740f880aaf3597b2c7
ayseadanc <adancaysee@gmail.com>
2022-11-22 18:24:09 +0300

    colormyviews project added

---
+++ colormyviews/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.udacity.colormyviews"
+    compileSdk = Config.compileSdkVersion
+
+    defaultConfig {
+        applicationId = "com.udacity.colormyviews"
+        minSdk = Config.minSdkVersion
+        targetSdk = Config.targetSdkVersion
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+
+}
+
+dependencies {
+
+    implementation(Libraries.androidxCore)
+    implementation(Libraries.androidxAppcompat)
+    implementation(Libraries.material)
+    implementation(Libraries.constraintLayout)
+
+    testImplementation(TestLibraries.junit)
+
+    androidTestImplementation(AndroidTestLibraries.junit)
+    androidTestImplementation(AndroidTestLibraries.espressoCore)
+}
