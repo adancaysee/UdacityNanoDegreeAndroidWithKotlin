740abb6af4a74d090bc7e3f2222ba8be919cca67
Ayşe Adanç <adancaysee@gmail.com>
2023-01-17 19:16:41 +0100

    marsrealestate module added

---
+++ marsrealestate/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    namespace = "com.udacity.marsrealestate"
+    compileSdk = Config.compileSdkVersion
+
+    defaultConfig {
+        applicationId = "com.udacity.marsrealestate"
+        minSdk = Config.minSdkVersion
+        targetSdk = Config.targetSdkVersion
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(Libraries.androidxCore)
+    implementation(Libraries.androidxAppcompat)
+    implementation(Libraries.material)
+    implementation(Libraries.constraintLayout)
+
+    implementation(Libraries.androidxNavigationUi)
+    implementation(Libraries.androidxNavigationFragment)
+
+    implementation(Libraries.androidxRoomRuntime)
+    implementation(Libraries.androidxRoomKtx)
+    kapt(Libraries.androidxRoomCompiler)
+
+    implementation(Libraries.timber)
+
+    testImplementation(TestLibraries.junit)
+
+    androidTestImplementation(AndroidTestLibraries.junit)
+    androidTestImplementation(AndroidTestLibraries.espressoCore)
+    androidTestImplementation(AndroidTestLibraries.room)
+    androidTestImplementation(AndroidTestLibraries.coroutine)
+}
