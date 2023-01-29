fe352ccdfd7365926d9a53f5c6dcd18595df33ff
Ayşe Adanç <adancaysee@gmail.com>
2023-01-29 09:28:33 +0100

    added fab and fab's click action

--- gdgfinder/src/main/java/com/udacity/gdgfinder/home/HomeFragment.kt
+++ gdgfinder/src/main/java/com/udacity/gdgfinder/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.gdgfinder.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -20,6 +21,19 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner) {
+            it?.let {
+                if (it) {
+                    findNavController().navigate(HomeFragmentDirections.actionHomeDestinationToGdgListDestination())
+                    viewModel.onNavigatedToSearch()
+                }
+            }
+
+        }
+
         return binding.root
     }
 }
