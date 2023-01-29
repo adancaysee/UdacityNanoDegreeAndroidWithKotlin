ecacfd6108e4f895743ff7be544cb90cd4867c59
Ayşe Adanç <adancaysee@gmail.com>
2023-01-29 12:35:57 +0100

    added textAppearance and theme overlay

---
+++ gdgfinder/src/main/java/com/udacity/gdgfinder/tutorial/split_files.kt
@@ -0,0 +1,9 @@
+package com.udacity.gdgfinder.tutorial
+
+/**
+ * themes.xml ==> Themes and ThemeOverlays
+ * type.xml   ==> TextAppearance and text dimension size
+ * styles.xml ==> Widget style
+ * shape.xml  ==> ShapeAppearances
+ * system_ui.xml ==> booleans,colors used to control system ui
+ */
---
+++ gdgfinder/src/main/java/com/udacity/gdgfinder/tutorial/theme_vs_style.kt
@@ -0,0 +1,20 @@
+package com.udacity.gdgfinder.tutorial
+
+/**
+ * Style
+  Map<view attribute, resource> --> key-value stores
+  Collection of view attributes
+  Associated with single a view
+  Usage -- it can be used from any individual view
+  Scope -- A style applied to a view only applies to that view, not to any of its children
+  Theme
+  Map<theme attribute, resource> --> key-value stores
+  Not: Theme attributes are different from view attributes because they’re not properties specific to an individual view type
+  Collection of named resources
+  Associated with context (Application,activity or view/viewgroups)
+  Overlay ancestors
+  useful broadly across an app -- all ap
+  Scope -- A Theme is accessed as a property of a Context and can be obtained from any object which is or has a Context
+ *
+ *
+ * */
