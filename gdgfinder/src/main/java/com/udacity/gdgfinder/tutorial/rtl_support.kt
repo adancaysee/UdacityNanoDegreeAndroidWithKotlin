3e39285e9eb5b3f78e060280f892f83c7fc5279c
Ayşe Adanç <adancaysee@gmail.com>
2023-01-29 14:26:19 +0100

    solved bugs for rtl support

---
+++ gdgfinder/src/main/java/com/udacity/gdgfinder/tutorial/rtl_support.kt
@@ -0,0 +1,16 @@
+package com.udacity.gdgfinder.tutorial
+
+/**
+ * RTL API -- (Some view components need further customization)
+ *  - android:layoutDirection
+ *  - android:textDirection
+ *  - android:textAlignment
+ *  - getLayoutDirectionFromLocale()
+ *
+ *  FOR RTL bugs
+ *  - Replace left,right with start end (exm: marginLeft -> marginStart)
+ *  - Remove auto-mirror
+ *
+ *  RTL INTEGRATION
+ *  -- Add support to manifest ( android:supportsRtl="true" , this comes default when creating an project)
+ */
