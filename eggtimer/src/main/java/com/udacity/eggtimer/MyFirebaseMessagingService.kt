490dd583c566c9a9946b1e0267b56bad5f622254
Ayşe Adanç <adancaysee@gmail.com>
2023-02-19 15:43:45 +0100

    added and subscribed to topic

--- eggtimer/src/main/java/com/udacity/eggtimer/MyFirebaseMessagingService.kt
+++ eggtimer/src/main/java/com/udacity/eggtimer/MyFirebaseMessagingService.kt
@@ -2,11 +2,23 @@ package com.udacity.eggtimer
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.udacity.eggtimer.util.getNotificationManager
+import com.udacity.eggtimer.util.sendNotification
 import timber.log.Timber
 
 /**
  * If app is background -- fcm send a notification
- * If app is foreground -- onMessageReceived is triggered
+ *  1. Data message --> Show default notification and onMessageReceived is not  triggered
+ *  2. Notification message --> Show default notification and onMessageReceived is not triggered
+ *
+ * ****
+ *
+ * If app is foreground
+ *  Not showing default notification
+ *  onMessageReceived is triggered  both data and notification message
+ *    1. Data message --> for data, (message.data != null)
+ *    2. Notification message --> (message.notification != null)
+ *                            --> for notification (You can show your custom notification with this object)
  */
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -16,5 +28,20 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        Timber.d("Message data payload: ${message.data}")
+
+        message.notification?.let {
+            sendNotification(it.body)
+        }
+
+
+    }
+
+    private fun sendNotification(messageBody:String?) {
+        getNotificationManager(this).sendNotification(
+            this,
+            messageBody ?: "from MyFirebaseMessagingService",
+        )
     }
 }
--- eggtimer/src/main/java/com/udacity/eggtimer/ui/EggTimerFragment.kt
+++ eggtimer/src/main/java/com/udacity/eggtimer/ui/EggTimerFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.messaging.FirebaseMessaging
+import com.udacity.eggtimer.R
 import com.udacity.eggtimer.databinding.FragmentEggTimerBinding
 
-
+private const val TOPIC = "breakfast"
 class EggTimerFragment : Fragment() {
 
     private val viewModel: EggTimerViewModel by lazy {
@@ -25,6 +28,19 @@ class EggTimerFragment : Fragment() {
         binding.eggTimerViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
+        subscribeTopic()
+
         return binding.root
+    }
+
+    private fun subscribeTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC).addOnCompleteListener {task->
+            val message = if (task.isSuccessful) {
+                getString(R.string.message_subscribed)
+            }else{
+                getString(R.string.message_subscribe_failed)
+            }
+            Toast.makeText(requireContext(),message,Toast.LENGTH_LONG).show()
+        }
     }
 }
