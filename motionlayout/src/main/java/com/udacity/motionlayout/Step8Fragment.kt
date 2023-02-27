cdc0a3f077a50d0f71c91db189862f9588fd1347
Ayşe Adanç <adancaysee@gmail.com>
2023-02-27 20:15:46 +0100

    added step8 animation

--- motionlayout/src/main/java/com/udacity/motionlayout/Step8Fragment.kt
+++ motionlayout/src/main/java/com/udacity/motionlayout/Step8Fragment.kt
@@ -4,18 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.udacity.motionlayout.databinding.FragmentStep7Binding
+import com.google.android.material.appbar.AppBarLayout
+import com.udacity.motionlayout.databinding.FragmentStep8Binding
+
+/**
+ * We did CollapsingToolbar effect with MotionLayout
+ */
+
 
 class Step8Fragment : Fragment() {
+
+    private lateinit var binding: FragmentStep8Binding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentStep7Binding.inflate(inflater, container, false)
+        (requireActivity() as AppCompatActivity).supportActionBar!!.hide()
+        binding = FragmentStep8Binding.inflate(inflater, container, false)
 
+        coordinateMotion()
 
         return binding.root
+    }
+
+    private fun coordinateMotion() {
+        val listener = AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+            val seekPosition = -verticalOffset / binding.appbarLayout.totalScrollRange.toFloat()
+            binding.motionLayout.progress = seekPosition
+        }
+        binding.appbarLayout.addOnOffsetChangedListener(listener)
     }
 }
