2a2142786d77591274e9d2fe0e52a1cfa07323cb
Ayşe Adanç <adancaysee@gmail.com>
2023-03-12 15:27:22 +0100

    first step for test

--- todo/src/test/java/com/udacity/todo/ExampleUnitTest.kt
+++ todo/src/test/java/com/udacity/todo/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.udacity.todo
 
+import androidx.core.math.MathUtils
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +13,7 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val result = MathUtils.multiplyExact(3,2)
+        assertEquals(6,result)
     }
 }
---
+++ todo/src/test/java/com/udacity/todo/Tutorial.kt
@@ -0,0 +1,33 @@
+package com.udacity.todo
+
+/**
+ * TESTING TYPE
+ * 1. Manuel Testing -> Human
+ * 2. Automated Test -> Machine and code
+ *
+ * READABLE TESTING
+ * 1. Naming -> subjectUnderTest_actionOrInput_resultState
+ * 2. GIVEN/WHEN/THEN --> add comment to test function
+ * 3. Assertion Framework --> Truth,Hamcrest
+ *
+ * AUTOMATED TEST
+ * 1.UNIT TEST ( %70 )
+ *   Scope of a single action or class
+ *   Helps pinpoint failure (You know exactly when in your code the issue is)
+ *   Should run fast,usually local
+ *   Low fidelity (the app is more than an execution of one method or class)
+ *
+ * 2.INTEGRATION TEST ( %20 )
+ *  Several classes or a single feature
+ *  Ensure class can work together
+ *  Larger scope than unit test
+ *  Can be local or instrumented test
+ *
+ * 3.END-TO-END TEST ( %10 )
+ *  Large portions of the app
+ *  Combination of features working together
+ *  High fidelity
+ *  Test that the app works as a whole
+ *  Simulate real usage,almost always instrumented test
+ *
+ */
